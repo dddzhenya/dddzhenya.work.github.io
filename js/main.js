6709648de67e4c9bf8017c5a4db2c1f9e60f55a5
zheny <eugeniapareeva@yandex.ru>
2023-02-12 23:22:11 +0300

    work

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+  const swiper = new Swiper('.swiper', {
+    /* autoplay: {
+      delay: 4000,
+      disableOnInteraction: false}, */
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  });
+
+
